c01213dee816034f129344d0b4f931fbd33374f1
Warriorid <i.hockeyakk@fmail.com>
2025-11-14 02:25:17 +0300

    update model user

--- internal/models/teamMember.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type TeamMember struct {
-    UserID   string `json:"user_id" binding:"required"`
-    Username string `json:"username" binding:"required"`
-    TeamID   int    `json:"team_id" binding:"required"`
-    IsActive bool   `json:"is_active" binding:"required"`
-}
